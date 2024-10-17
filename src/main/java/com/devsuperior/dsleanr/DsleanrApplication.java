c5a6f62711c0f76f3a8ee956c0ecded068d5f7f7
mariomoreirasilva <mario@desban.org.br>
2024-10-17 09:54:02 -0300

    Entidade Role e Usuario

---
+++ src/main/java/com/devsuperior/dsleanr/DsleanrApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsleanr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsleanrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsleanrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dsleanr/DsleanrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsleanr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsleanrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
