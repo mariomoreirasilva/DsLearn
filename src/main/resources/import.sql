a02a722fde265481bb5ec96e44d1226cc31ac044
mariomoreirasilva <mario@desban.org.br>
2024-10-17 11:00:49 -0300

    Entidade Course

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,3 +12,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+
+INSERT INTO tb_course(name, img_Uri, img_Gray_Uri) VALUES('Bootcamp HTML', 'https://www.femerj.org.br/wp-content/uploads/2024/06/image-from-rawpixel-id-5913662-jpeg-1-930x620.jpg', 'https://pepsic.bvsalud.org/img/revistas/pef/v7n1/n1a05t3.JPG'); 
